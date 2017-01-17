5a03a19083442c2954f104535e61f5cd570b477f
Rob Basilona <basilona.jr.b@gmail.com>
2017-01-17 19:41:46 +0800

    added gmaps for first page

--- mobile/src/pages/home/home.ts
+++ mobile/src/pages/home/home.ts
@@ -1,36 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
 
 import { Geolocation } from 'ionic-native';
+
+declare var google;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
   public biscuit: Boolean;
   public water: Boolean;
   public goods: Boolean;
   public batteries: Boolean;
-  public lat: Number;
-  public lon: Number;
+  public lat: number;
+  public lon: number;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public platform: Platform) {
     this.biscuit = true;
     this.water = false;
     this.goods = true;
     this.batteries = false;
-    this.geolocate();
+    // this.loadMap();
   }
 
-  geolocate(){
-    Geolocation.getCurrentPosition().then((resp) => {
-      this.lat = resp.coords.latitude;
-      this.lon = resp.coords.longitude;
-    }).catch((error) => {
-      console.log('Error getting location', error);
+  ionViewDidLoad(){
+    this.loadMap();
+  }
+
+  loadMap(){
+    Geolocation.getCurrentPosition().then((position) => {
+      this.lat = position.coords.latitude;
+      this.lon = position.coords.longitude;
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+      this.addMarker();
+    }, (err) => {
+      console.log(err);
     });
   }
 
+  addMarker(){
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+    let content = "<h4>Information!</h4>";
+    this.addInfoWindow(marker, content);
+  }
+
+  addInfoWindow(marker, content){
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+  }
 }
