726645d07683b4823766270cf935dbc432556001
Sean Ryan Chan <seanryachan@gmail.com>
2017-01-19 23:20:53 +0800

    Fixed padding issues

--- mobile/src/pages/splash/splash.ts
+++ mobile/src/pages/splash/splash.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, LoadingController } from 'ionic-angular';
+import { NavController, NavParams} from 'ionic-angular';
 import { TabsPage } from '../tabs/tabs';
+import { SMS } from 'ionic-native';
 
 /*
   Generated class for the Splash page.
@@ -14,53 +15,7 @@ import { TabsPage } from '../tabs/tabs';
 })
 export class SplashPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public loadingCtrl: LoadingController) {}
-
-  presentLoadingDefault() {
-  let loading = this.loadingCtrl.create({
-    content: 'Please wait...'
-  });
-
-  loading.present();
-
-  setTimeout(() => {
-    loading.dismiss();
-  }, 5000);
-}
-
-presentLoadingCustom() {
-  let loading = this.loadingCtrl.create({
-    spinner: 'hide',
-    content: `
-      <div class="custom-spinner-container">
-        <div class="custom-spinner-box"></div>
-      </div>`,
-    duration: 5000
-  });
-
-  loading.onDidDismiss(() => {
-    console.log('Dismissed loading');
-  });
-
-  loading.present();
-}
-
-presentLoadingText() {
-  let loading = this.loadingCtrl.create({
-    spinner: 'hide',
-    content: 'Loading Please Wait...'
-  });
-
-  loading.present();
-
-  setTimeout(() => {
-    this.navCtrl.push(TabsPage);
-  }, 1000);
-
-  setTimeout(() => {
-    loading.dismiss();
-  }, 5000);
-}
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   goTo(){
     this.navCtrl.push(TabsPage);
