2f758788d337af8d980753ace4370c1b29790cf0
Joseph Earl Branzuela <jebranzuela@gmail.com>
2017-01-19 20:44:17 +0800

    Edited chikka_controller.rb

--- railsapi/app/models/evac_center.rb
+++ railsapi/app/models/evac_center.rb
@@ -1,17 +1,24 @@
 include Geokit::Geocoders
 
 class EvacCenter < ApplicationRecord
-	acts_as_mappable
+	acts_as_mappable :default_units => :kms,
+                   	 :default_formula => :sphere,
+					 :lat_column_name => :latitude,
+                     :lng_column_name => :longitude
 
-	class << self
-		def get_result(msg)
-		 	# rever geo(q)
-		 	#latlng
-		 	#compare latlng to db
-		 	#Geokit::given two points, compare distance
-		 	#get feasible solutions
-			geocoder = GoogleGeocoder.geocode(msg)
-		return geocoder
+	def self.get_result(msg)
+	 	# rever geo(q)
+	 	#latlng
+	 	#compare latlng to db
+	 	#Geokit::given two points, compare distance
+	 	#get feasible solutions
+		# geocoder = GoogleGeocoder.geocode(msg)
+		results =  EvacCenter.within(10, :origin=>msg)
+		response = "We recommend you to take shelter on these areas: "
+
+		results.each do |res|
+			response += res.name + "; "
 		end
+		response
 	end
 end
