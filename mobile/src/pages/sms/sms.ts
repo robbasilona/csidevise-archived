ed54e0c4b4a0418f3234e0f45f3263684711f0d2
Sean Ryan Chan <seanryachan@gmail.com>
2017-01-20 15:07:26 +0800

    Fixed Styles

--- mobile/src/pages/sms/sms.ts
+++ mobile/src/pages/sms/sms.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import { Validators, FormBuilder } from '@angular/forms';
+import { SMS } from 'ionic-native';
 /*
-  Generated class for the Sms page.
+Generated class for the Sms page.
 
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
@@ -12,11 +13,21 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'sms.html'
 })
 export class SmsPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
-
+  textfield: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder) {
+    this.textfield = this.formBuilder.group({
+      textfielder: ['', Validators.required]
+    });
+  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad SmsPage');
   }
 
+  logForm(){
+    console.log(this.textfield.value);
+  }
+
+  sendSMS(){
+    SMS.send('29290469148', this.textfield.value);
+  }
 }
